80ecb08e1a6b5ed30ccef1abedcd4aea7655c6f0
unknown <caglabircan@hotmail.com>
2022-10-24 21:08:20 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(cb) {
+    return cb();
+}
+
+function returnsANamedFunction() {
+    return function name() {
+        return
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        return
+    }
+}
